a0a34afbc07f6fb48b0a394c8b9c8706cb79b321
ilianoKokoro <iliandelagrange@gmail.com>
2026-06-29 16:20:42 -0400

    Found material3 version with the issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -37,7 +36,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
@@ -51,10 +50,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material.icons.extended)
-
-    implementation(libs.nav3.runtime)
-    implementation(libs.nav3.ui)
-
-    implementation(libs.kotlinx.serialization.core)
-    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/ca/ilianokokoro/backcrashrepro/MainActivity.kt
+++ app/src/main/java/ca/ilianokokoro/backcrashrepro/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,28 +15,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation3.runtime.NavEntry
-import androidx.navigation3.runtime.NavKey
-import androidx.navigation3.runtime.rememberNavBackStack
-import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
-import androidx.navigation3.ui.NavDisplay
 import ca.ilianokokoro.backcrashrepro.ui.theme.BackCrashReproTheme
-import kotlinx.serialization.Serializable
-
-@Serializable
-data object FirstScreen : NavKey
-
-@Serializable
-data object SecondScreen : NavKey
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,76 +30,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             BackCrashReproTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        NavigationRoot()
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun NavigationRoot() {
-    val backStack = rememberNavBackStack(FirstScreen)
-
-    NavDisplay(
-        modifier = Modifier
-            .fillMaxSize(),
-        backStack = backStack,
-        onBack = backStack::removeLastOrNull,
-        entryDecorators = listOf(
-            rememberSaveableStateHolderNavEntryDecorator(),
-        ),
-        entryProvider = { key ->
-            when (key) {
-
-                is FirstScreen -> NavEntry(key) {
-                    Column {
-                        TextButton(onClick = { backStack.add(SecondScreen) }) {
-                            Text("Go to screen 2")
+                    var expanded by remember { mutableStateOf(false) }
+                    
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false },
+                            shape = RoundedCornerShape(24.dp),
+                        ) {
+                            Text("Crash contents")
+                        }
+                        IconButton(onClick = { expanded = true }) {
+                            Icon(
+                                imageVector = Icons.Rounded.MoreVert,
+                                contentDescription = null
+                            )
                         }
                     }
                 }
-
-                is SecondScreen -> NavEntry(key) {
-                    Column {
-                        TextButton(onClick =  backStack::removeLastOrNull ) {
-                            Text("Back")
-                        }
-                    CrashDropdown()
-                    }
-                }
-
-
-                else -> throw RuntimeException()
             }
-        })
-}
-
-
-@Composable
-fun CrashDropdown() {
-    var expanded by remember { mutableStateOf(false) }
-    Column {
-        IconButton(onClick = { expanded = true }) {
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = null
-            )
-        }
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            shape = RoundedCornerShape(24.dp),
-        ) {
-            Text("Crash contents")
         }
     }
 }
